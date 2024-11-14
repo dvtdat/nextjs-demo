247d50d50561b97344045f3a002e3dfbec017acf
dvtdat <tiendat0417@gmail.com>
2024-11-14 22:31:43 +0700

    chore: add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,11 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+const ubuntu = localFont({
+  src: "./fonts/Ubuntu.woff",
+  variable: "--font-ubuntu",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${ubuntu.variable} antialiased`}
       >
         {children}
       </body>
